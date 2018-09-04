5f73984321bd834eb1c5865fe4851cf4ce78c3df
Darklip <1darkpower1@gmail.com>
2018-09-04 18:50:08 +0400

    init

---
+++ resources/js/pageScript.js
@@ -0,0 +1,14 @@
+var emailName = '1darkpower1';
+var emailDomain = 'gmail.com';
+
+function writeEmail() {
+    document.write(emailName + '@' + emailDomain);
+}
+
+function contactAdmin() {var emailName = '1darkpower1';
+    window.open("mailto:"+emailName + '@' + emailDomain);
+}
+
+jQuery(document).ready(function () {
+    
+});
